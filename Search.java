45f61b6e6c1f86dd7891f84992347460d562f01f
AyaAshraf21 <92271264+AyaAshraf21@users.noreply.github.com>
2023-05-10 04:21:02 +0300

    Update Search ----> final

--- Search.java
+++ Search.java
@@ -2,14 +2,53 @@ package Shopping;
 
 
 import java.util.ArrayList;
+import java.util.Formatter;
+import Register_Login.Database;
+
 public class Search {
     private ArrayList <Item> items ;
+    private Database db = new Database();
+
+    public void DisplayResult(){
+        Formatter formatter = new Formatter();
+        String line ;
+        line = String.format("%53s", "").replace(" ", "-");
+        System.out.println(line);
+        formatter.format("|%-12s|%-12s|%12s|%12s|\n", "ID" ,"Name", "Brand", "Quantity");
+        System.out.print(formatter.toString());
+        System.out.print("|");
+        line = String.format("%51s", "").replace(" ", "-");
+        System.out.println(line+ "|");
+
+        for (Item item : items) {
+            formatter = new Formatter();
+            formatter.format("|%-12s|%-12s|%12s|%12d|\n", item.getID() , item.getName(), item.getBrand(), item.getQuantity());
+            System.out.print(formatter.toString());
+        }
+        line = String.format("%53s", "").replace(" ", "-");
+        System.out.println(line);
+    }
 
 
     //should work on items provided from DB
-    // public Item Research(String Query){
+    public void Research(String query) {
+        this.items = db.search(query);
+        DisplayResult();
+    }
 
-    // }
+    public void SelectItem(String id){
+        boolean found = false;
+        for(Item item:items){
+            if(id.equals(item.getID())){
+                System.out.println("ID: "+item.getID() + "\nName: " +item.getName() +"\nQuantity: "+item.getQuantity() +"\nPrice: "+ item.getPrice() +"\nDiscount percentage: "+item.getDiscountPercent()+"%\nBrand: " +item.getBrand() + "\nDescription : " +item.getDescription());
+                found = true;
+            }
+        }
+        if(!found){
+            System.out.println("invalid id , please try agian");
+        }
+    }
+
 
 
 }
