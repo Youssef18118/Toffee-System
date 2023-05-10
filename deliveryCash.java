32bdbe8c9b86996f2d5b4b4c873df96f50d32e15
Abdelrhman Tarek <47614199+Abdelrhman-T@users.noreply.github.com>
2023-05-11 00:15:10 +0300

    Update deliveryCash.java

--- deliveryCash.java
+++ deliveryCash.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package payment;
+import Users.*;
 
 /**
  *
@@ -10,13 +11,24 @@ package payment;
  */
 public class deliveryCash {
     private String phoneNO;
+    private loggedUser userX;
 
     public String getPhoneNO() {
         return phoneNO;
     }
 
     public void setPhoneNO(String phoneNO) {
-        this.phoneNO = phoneNO;
+        this.phoneNO = userX.getPhoneNO();
     }
 
+    public void setUserX(loggedUser userX) {
+        this.userX.setID(userX.getID());
+        this.userX.setAddress(userX.getAddress());
+        this.userX.setEmail(userX.getEmail());
+        this.userX.setUserName(userX.getUserName());
+        this.userX.setPassword(userX.getPassword());
+        this.userX.setLoyalityPoints(userX.getLoyalityPoints());
+        this.userX.setPhoneNO(userX.getPhoneNO());
+    }
+    
 }
