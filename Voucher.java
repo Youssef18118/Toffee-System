c2c109c461cd3c36b0f81e57e5814aaa0798089e
Abdelrhman Tarek <47614199+Abdelrhman-T@users.noreply.github.com>
2023-05-11 01:06:53 +0300

    Update Voucher.java

--- Voucher.java
+++ Voucher.java
@@ -12,7 +12,8 @@ public class Voucher {
     private int price;
     private String EXP;
     private String code;
-
+    private loggedUser userX = new loggedUser();
+    
     public int getPrice() {
         return price;
     }
@@ -37,5 +38,13 @@ public class Voucher {
         this.code = code;
     }
     
-    
+    public void setUserX(loggedUser userX) {
+        this.userX.setID(userX.getID());
+        this.userX.setAddress(userX.getAddress());
+        this.userX.setEmail(userX.getEmail());
+        this.userX.setUserName(userX.getUserName());
+        this.userX.setPassword(userX.getPassword());
+        this.userX.setLoyalityPoints(userX.getLoyalityPoints());
+        this.userX.setPhoneNO(userX.getPhoneNO());
+    }
 }
