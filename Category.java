d4d8d2191da43683495888faca1bee8376e8a9fd
AyaAshraf21 <92271264+AyaAshraf21@users.noreply.github.com>
2023-05-10 23:04:54 +0300

    Update Category ----> final

--- Category.java
+++ Category.java
@@ -1,18 +1,20 @@
 package Shopping;
 
 
+import Register_Login.Database;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-public class Category {
-    private ArrayList <Category> Catg ;
-    private int ID;    
-    private String Name;
 
+
+public class Category {
+    private ArrayList <String> Catg = new ArrayList<>() ;
+    private Database db = new Database() ;
+    private int ID;
+    private String Name;
     // CategoryName : ID of items
-    Map<String, ArrayList<Integer>> CategoryMap = new HashMap<>();
+    Map<String, ArrayList<Item>> CategoryMap = new HashMap<>();
 
 
     public String getCategoryName(){
@@ -22,20 +24,39 @@ public class Category {
         return ID;
     }
 
-    public void setCategoryName(String Name, int ItemID){
-        for (Category category : Catg) {
-            if(category.Name == Name){
-                //if found in list of Category, put it in the map
-                CategoryMap.get(Name).add(ItemID);    
-            }else{
-                //if not found in list of Category, add it and put it in the map
-                Catg.add(category);
-                CategoryMap.put(Name, new ArrayList<>(List.of(ItemID)));   
-            }
+
+    public void setCategoryMap(){
+        db.add_to_Category(CategoryMap);
+        for (Map.Entry<String, ArrayList<Item>> entry : CategoryMap.entrySet()) {
+            String key = entry.getKey();
+            Catg.add(key);
+        }
+        for(int i=0 ; i<Catg.size() ;i++){
+            System.out.println(i+1 + "- "+ Catg.get(i));
         }
 
-        
-        this.Name = Name;
+    }
+
+    public void selectCategory(int index){
+        Search search = new Search();
+        if (CategoryMap.containsKey(Catg.get(index-1))) {
+            ArrayList<Item> list = CategoryMap.get(Catg.get(index-1));
+            search.DisplayResult(list);
+        }
+    }
+
+    public void selectItem(int idxOfCatg ,String id){
+        boolean found = false;
+        ArrayList<Item> list = CategoryMap.get(Catg.get(idxOfCatg-1));
+        for(Item item:list){
+            if(id.equals(item.getID())){
+                item.viewItem();
+                found = true;
+            }
+        }
+        if(!found){
+            System.out.println("invalid id , please try agian");
+        }
     }
 
 
