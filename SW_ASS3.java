93212dfdf749b771d6f6388bd5eea46f2c7d05b2
Abdelrhman Tarek <47614199+Abdelrhman-T@users.noreply.github.com>
2023-05-12 17:21:14 +0300

    Update SW_ASS3.java

--- SW_ASS3.java
+++ SW_ASS3.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package payment;
 
-/**
- *
- * @author Boda_Tarek
- */
 public class SW_ASS3 {
 
     public static void main(String[] args) {
