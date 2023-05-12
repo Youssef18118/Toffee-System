774bb44ebd6733603316b779c31e6c2515d7bba4
Abdelrhman Tarek <47614199+Abdelrhman-T@users.noreply.github.com>
2023-05-12 17:22:32 +0300

    Update user.java

--- user.java
+++ user.java
@@ -1,14 +1,5 @@
 package Users;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Boda_Tarek
- */
 public class user {
     private int ID;
 
