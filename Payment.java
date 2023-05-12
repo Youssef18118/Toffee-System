f51e24f0089ecf8846c6ee670e26e1cd3147ad72
Abdelrhman Tarek <47614199+Abdelrhman-T@users.noreply.github.com>
2023-05-12 17:20:35 +0300

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package payment;
 import java.util.List;
 import Shopping.*;
 import Users.*;
 
-
-
-
-/**
- *
- * @author Boda_Tarek
- */
 public class Payment {
     private String address;
     private Payment method;
