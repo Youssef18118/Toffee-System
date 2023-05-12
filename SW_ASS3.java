5a21931f744609c621bc8332af68a23563dc4f45
Abdelrhman Tarek <47614199+Abdelrhman-T@users.noreply.github.com>
2023-05-12 17:21:30 +0300

    Update SW_ASS3.java

--- SW_ASS3.java
+++ SW_ASS3.java
@@ -4,6 +4,6 @@ package payment;
 public class SW_ASS3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Toffee!");
     }
 }
