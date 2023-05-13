1a849593f16d9e8706f1515b6cc50804dedaae69
Abdelrhman Tarek <47614199+Abdelrhman-T@users.noreply.github.com>
2023-05-13 15:01:25 +0300

    Update Category.java

--- Category.java
+++ Category.java
@@ -1,10 +1,13 @@
 package Shopping;
 import TSystem.*;
+import Users.loggedUser;
 
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
 
 
 public class Category {
@@ -14,7 +17,27 @@ public class Category {
     private String Name;
     // CategoryName : ID of items
     Map<String, ArrayList<Item>> CategoryMap = new HashMap<>();
+    Scanner in = new Scanner(System.in);
+    private Cart cart = new Cart();
+    private loggedUser userX = new loggedUser();
 
+    
+    
+    
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+
+
+    
+    
+    
+    
 
     public String getCategoryName(){
         return Name;
@@ -51,11 +74,21 @@ public class Category {
             if(id.equals(item.getID())){
                 item.viewItem();
                 found = true;
+                System.out.println("Do you want to add this item to the cart? 1 -> Yes, 2 -> No");
+                int response = in.nextInt();
+                if (response == 1) {
+            
+                    cart.addToCart(userX, item);
+                    cart.ViewCart(userX);
+                } else {
+                    break;
+                }
             }
         }
         if(!found){
             System.out.println("invalid id , please try agian");
         }
+
     }
 
 
