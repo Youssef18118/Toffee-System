fbf1036027bcb637910fc3f9f0b76448bec5f5cf
Abdelrhman Tarek <47614199+Abdelrhman-T@users.noreply.github.com>
2023-05-13 14:36:16 +0300

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,14 +1,8 @@
 package Shopping;
 
-<<<<<<< HEAD
 import Users.loggedUser;
 import Users.user;
 
-=======
-import Users.*;
-
-import java.util.ArrayList;
->>>>>>> 34bbfbcb1dc5ea552c50666e2fbd9f441589ec7a
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Formatter;
@@ -16,13 +10,8 @@ import java.util.Formatter;
 public class Cart {
     private float totalPrice;
     private int CartID;
-<<<<<<< HEAD
     public Map<Item, Integer> CartMap = new HashMap<>();
     private loggedUser userX = new loggedUser();
-=======
-    public Map<Item , Integer> CartMap = new HashMap<>();
-
->>>>>>> 34bbfbcb1dc5ea552c50666e2fbd9f441589ec7a
 
     public loggedUser getUserX() {
         return userX;
@@ -42,7 +31,6 @@ public class Cart {
         return CartID;
     }
 
-<<<<<<< HEAD
     public float gettotalPrice() {
         return totalPrice;
     }
@@ -52,41 +40,15 @@ public class Cart {
         CartMap.put(Itemx, 1);
     }
 
-    public void ViewCart(user userx) {
-        if (userx instanceof loggedUser) {
-            for (Map.Entry<Item, Integer> entry : CartMap.entrySet()) {
-                Formatter formatter = new Formatter();
-                formatter.format("|%-12s|%-12s|%-12d|\n", entry.getKey().getID(), entry.getKey().getName(),
-                        entry.getValue());
-=======
-    public float gettotalPrice(){
-        return totalPrice;
-    }
-    
-   
-    public void addItem(user userx ,Item Itemx){
-        if(userx instanceof loggedUser){
-            CartMap.put(Itemx , 0);
+    public void ViewCart(loggedUser userx) {
+        for (Map.Entry<Item, Integer> entry : CartMap.entrySet()) {
+            Formatter formatter = new Formatter();
+            formatter.format("|%-12s|%-12s|%-12d|\n", entry.getKey().getID(), entry.getKey().getName(),
+                    entry.getValue());
+            System.out.print(formatter.toString());
         }
-        else{
-            System.out.println("Sorry , you must login first");
-        }
-    }
-
-    public void ViewCart(user userx){
-        if(userx instanceof loggedUser){
-            for (Map.Entry<Item, Integer> entry : CartMap.entrySet()) {
-                Formatter formatter = new Formatter();
-                formatter.format("|%-12s|%-12s|%-12d|\n", entry.getKey().getID() , entry.getKey().getName() , entry.getValue());
->>>>>>> 34bbfbcb1dc5ea552c50666e2fbd9f441589ec7a
-                System.out.print(formatter.toString());
-            }
-            String line = String.format("%53s", "").replace(" ", "-");
-            System.out.println(line);
-<<<<<<< HEAD
-        } else {
-            System.out.println("Sorry, you must login first");
-        }
+        String line = String.format("%53s", "").replace(" ", "-");
+        System.out.println(line);
     }
 
     public void selectQuantity(String ID, int Quant) {
@@ -113,32 +75,4 @@ public class Cart {
         }else
            return false;
     }
-=======
-        }
-        else{
-            System.out.println("Sorry , you must login first");
-        }
-    }
-
-
-
-
-     public void selectQuantitiy (String ID ,int Quant){
-         boolean found = false;
-         for (Map.Entry<Item, Integer> entry : CartMap.entrySet()){
-             if(ID.equals(entry.getKey().getID())){
-                 if((Quant > entry.getKey().getQuantity()) || (Quant > 50)){
-                     System.out.println("you exceeded the maximum number of items , please try again");
-                 }
-                 else{
-                     entry.setValue(Quant);
-                 }
-             }
-         }
-         if(!found){
-             System.out.println("invalid id , please try agian");
-         }
-     }
->>>>>>> 34bbfbcb1dc5ea552c50666e2fbd9f441589ec7a
 }
-
